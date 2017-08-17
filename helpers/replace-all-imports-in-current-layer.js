f1f4c8ede61d5a813cd294cecae43b08f825b379
viktor <baranov.viktor.27@gmail.com>
2017-08-17 18:38:18 +0300

    Broken order of imports strong fix

--- helpers/replace-all-imports-in-current-layer.js
+++ helpers/replace-all-imports-in-current-layer.js
@@ -8,26 +8,32 @@ const updateImportObjectLocationInTarget = require("./update-import-object-locat
 function replaceAllImportsInCurrentLayer(i, importObjs, updatedFileContent, dir, cb) {
 	if (i < importObjs.length) {
 		var importObj = importObjs[i];
+		importObj = updateImportObjectLocationInTarget(importObj, updatedFileContent);
 
 		//replace contracts aliases
 		if (importObj.contractName) {
 			updatedFileContent = updatedFileContent.replace(importObj.alias + ".", importObj.contractName + ".");
 		}
 		
-		importObj = updateImportObjectLocationInTarget(importObj, updatedFileContent);
 		var importStatement = updatedFileContent.substring(importObj.startIndex, importObj.endIndex);
 
 		var fileExists = fs.existsSync(dir + importObj.dependencyPath, fs.F_OK);
 		if (fileExists) {
+			console.log("###" + importObj.dependencyPath + " SOURCE FILE FOUND###");
 			var importedFileContent = fs.readFileSync(dir + importObj.dependencyPath, "utf8");
 			replaceRelativeImportPaths(importedFileContent, path.dirname(importObj.dependencyPath) + "/", function(importedFileContentUpdated) {
 				if (!variables.importedSrcFiles.hasOwnProperty(path.basename(dir + importObj.dependencyPath))) {
 					variables.importedSrcFiles[path.basename(dir + importObj.dependencyPath)] = importedFileContentUpdated;
-					updatedFileContent = updatedFileContent.replace(importStatement, importedFileContentUpdated);
+					if (importedFileContentUpdated.indexOf(" is ") > -1) {
+						updatedFileContent = updatedFileContent.replace(importStatement, importedFileContentUpdated);
+					} else {
+						updatedFileContent = updatedFileContent.replace(importStatement, "");
+						updatedFileContent = importedFileContentUpdated + updatedFileContent;
+					}
 				}
 				else {
 					updatedFileContent = updatedFileContent.replace(importStatement, "");
-					//todo: issue #1.
+					//issue #1.
 					if (updatedFileContent.indexOf(variables.importedSrcFiles[path.basename(dir + importObj.dependencyPath)] > -1)
 						&& updatedFileContent.indexOf("import ") == -1) {
 						updatedFileContent = updatedFileContent.replace(variables.importedSrcFiles[path.basename(dir + importObj.dependencyPath)], "");
@@ -39,11 +45,18 @@ function replaceAllImportsInCurrentLayer(i, importObjs, updatedFileContent, dir,
 				replaceAllImportsInCurrentLayer(i, importObjs, updatedFileContent, dir, cb);
 			});
 		} else {
-			console.log("!!!" + importObj.dependencyPath + " SOURCE FILE NOT FOUND. TRY TO FIND IT RECURSIVELY!!!");
-			findFile.byNameAndReplace(dir.substring(0, dir.lastIndexOf("/")), path.basename(importObj.dependencyPath), updatedFileContent, importStatement, function(_updatedFileContent) {
+			if (!variables.importedSrcFiles.hasOwnProperty(path.basename(dir + importObj.dependencyPath))) {
+				console.log("!!!" + importObj.dependencyPath + " SOURCE FILE NOT FOUND. TRY TO FIND IT RECURSIVELY!!!");
+				findFile.byNameAndReplace(dir.substring(0, dir.lastIndexOf("/")), path.basename(importObj.dependencyPath), updatedFileContent, importStatement, function(_updatedFileContent) {
+					i++;
+					console.log("###" + importObj.dependencyPath + " SOURCE FILE FOUND###");
+					replaceAllImportsInCurrentLayer(i, importObjs, _updatedFileContent, dir, cb);
+				});
+			} else {
+				updatedFileContent = updatedFileContent.replace(importStatement, "");
 				i++;
-				replaceAllImportsInCurrentLayer(i, importObjs, _updatedFileContent, dir, cb);
-			});
+				replaceAllImportsInCurrentLayer(i, importObjs, updatedFileContent, dir, cb);
+			}
 		}
 	} else cb(updatedFileContent);
 }
