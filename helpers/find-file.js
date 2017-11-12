6b69f175a904cfc6e1c8aee8b751863103d84c68
viktor <baranov.viktor.27@gmail.com>
2017-11-12 20:48:23 +0300

    refactor

--- helpers/find-file.js
+++ helpers/find-file.js
@@ -20,53 +20,76 @@ function byName(dir, fileName, cb) {
 	});
 }
 
-function byNameAndReplace(dir, fileName, updatedFileContent, importStatement, cb) {
+function byNameAndReplace(dir, filePath, updatedFileContent, importStatement, cb) {
 	glob(dir + "/**/*.sol", function(err, srcFiles) {
 		if (err) return console.log(err.message);
 		
 		var importIsReplacedBefore = false;
-		for (var j = 0; j < srcFiles.length; j++) {
-			if (path.basename(srcFiles[j]) == fileName) {
-				if (!variables.importedSrcFiles.hasOwnProperty(path.basename(srcFiles[j]))) {
-					var fileContent = fs.readFileSync(srcFiles[j], "utf8");
-					if (fileContent.indexOf(" is ") > -1) {
-						updatedFileContent = updatedFileContent.replace(importStatement, fileContent);
-					} else {
-						//updatedFileContent = updatedFileContent.replace(importStatement, fileContent);
-						updatedFileContent = updatedFileContent.replace(importStatement, "");
-						updatedFileContent = fileContent + updatedFileContent;
-					}
-					variables.importedSrcFiles[path.basename(srcFiles[j])] = fileContent;
-					cb(updatedFileContent);
-					return;
-				} else {
-					updatedFileContent = updatedFileContent.replace(importStatement, "");
-					//issue #2.
-					if (updatedFileContent.indexOf(variables.importedSrcFiles[path.basename(dir + importObj.dependencyPath)] > -1)
-						&& updatedFileContent.indexOf("import ") == -1) {
-						var fileContent = fs.readFileSync(srcFiles[j], "utf8");
-						updatedFileContent = updatedFileContent.replace(variables.importedSrcFiles[path.basename(dir + importObj.dependencyPath)], "");
-						updatedFileContent = fileContent + updatedFileContent;
-					}
-					importIsReplacedBefore = true;
-				}
-				break;
-			}
-		}
-
-		if (importIsReplacedBefore) {
-			updatedFileContent = updatedFileContent.replace(importStatement, "");
-			cb(updatedFileContent);
-		} else {
-			if (dir.indexOf("/") > -1) {
-				dir = dir.substring(0, dir.lastIndexOf("/"));
-				byNameAndReplace(dir, fileName, updatedFileContent, importStatement, cb);
-			} else {
+		byNameAndReplaceInner(importStatement, updatedFileContent, dir, filePath, srcFiles, 0, cb, function() {
+			if (importIsReplacedBefore) {
 				updatedFileContent = updatedFileContent.replace(importStatement, "");
 				cb(updatedFileContent);
+			} else {
+				if (dir.indexOf("/") > -1) {
+					dir = dir.substring(0, dir.lastIndexOf("/"));
+					byNameAndReplace(dir, filePath, updatedFileContent, importStatement, cb);
+				} else {
+					updatedFileContent = updatedFileContent.replace(importStatement, "");
+					cb(updatedFileContent);
+				}
 			}
-		}
+		})
 	});
+}
+
+function byNameAndReplaceInner(importStatement, updatedFileContent, dir, filePath, srcFiles, j, cb, cbInner) {
+	if (j >= srcFiles.length) return cbInner()
+	const findAllImportPaths = require("./find-all-import-paths.js");
+	let isAbsolutePath = filePath.indexOf(".") != 0		
+	if (isAbsolutePath && srcFiles[j].indexOf(filePath) > -1) {
+
+		if (!variables.importedSrcFiles.hasOwnProperty(path.basename(srcFiles[j]))
+			|| fs.existsSync(filePath)) {
+			let fileContent 
+			if (fs.existsSync(filePath)) fileContent = fs.readFileSync(filePath, "utf8")
+			else fileContent = fs.readFileSync(srcFiles[j], "utf8");
+
+			findAllImportPaths(dir, fileContent, function(_importObjs) {
+				//replace relative paths to absolute path for imports
+				for (var i = 0; i < _importObjs.length; i++) {
+					let _fullImportStatement = _importObjs[i].fullImportStatement
+					let srcFileDir = srcFiles[j].substring(0, srcFiles[j].lastIndexOf("/"));
+					_fullImportStatement = _fullImportStatement.replace(_importObjs[i].dependencyPath, srcFileDir + "/" + _importObjs[i].dependencyPath)
+					fileContent = fileContent.replace(_importObjs[i].fullImportStatement, _fullImportStatement)
+				}
+
+				if (fileContent.indexOf(" is ") > -1) {
+					updatedFileContent = updatedFileContent.replace(importStatement, fileContent);
+				} else {
+					//updatedFileContent = updatedFileContent.replace(importStatement, fileContent);
+					updatedFileContent = updatedFileContent.replace(importStatement, "");
+					updatedFileContent = fileContent + updatedFileContent;
+				}
+				variables.importedSrcFiles[path.basename(srcFiles[j])] = fileContent;
+				return cb(updatedFileContent);
+			});
+		} else {
+			updatedFileContent = updatedFileContent.replace(importStatement, "");
+			//issue #2.
+			if (updatedFileContent.indexOf(variables.importedSrcFiles[path.basename(dir + importObj.dependencyPath)] > -1)
+				&& updatedFileContent.indexOf("import ") == -1) {
+				var fileContent = fs.readFileSync(srcFiles[j], "utf8");
+				updatedFileContent = updatedFileContent.replace(variables.importedSrcFiles[path.basename(dir + importObj.dependencyPath)], "");
+				updatedFileContent = fileContent + updatedFileContent;
+			}
+			importIsReplacedBefore = true;
+			j++;
+			byNameAndReplaceInner(importStatement, updatedFileContent, dir, filePath, srcFiles, j, cb, cbInner)
+		}
+	} else {
+		j++;
+		byNameAndReplaceInner(importStatement, updatedFileContent, dir, filePath, srcFiles, j, cb, cbInner)
+	}
 }
 
 module.exports = {
